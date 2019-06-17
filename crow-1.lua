88eea728cea72f1af79ed94a89fd812db9304218
brian crabtree <tehn@monome.org>
2019-06-18 03:30:56 +0700

    crow-1

---
+++ crow-1.lua
@@ -0,0 +1,40 @@
+-- crow example 1
+-- setting output
+
+local volts = 0
+
+function init()
+  crow.output[1].volts = volts
+  screen.level(15)
+  screen.aa(0)
+  screen.line_width(1)
+end
+
+function redraw()
+  screen.clear()
+  screen.move(10,40)
+  screen.text("volts: "..string.format("%.3f",volts))
+  screen.update()
+end
+
+function enc(n,z)
+  if n==2 then
+    volts = util.clamp(-5,10,volts + z*1)
+    crow.output[1].volts = volts
+   elseif n==3 then
+    volts = util.clamp(-5,10,volts+ z*0.01)
+    crow.output[1].volts = volts
+  end
+  redraw()
+end 
+
+function key(n,z)
+  if n==2 and z==1 then
+    volts = 0
+    crow.output[1].volts = volts
+  elseif n==3 and z==1 then
+    volts = math.random(12) / 12
+    crow.output[1].volts = volts
+  end
+  redraw()
+end
