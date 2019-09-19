e1f02190379c25b9b58ff661871b8a45f2ee2b6c
brian crabtree <tehn@monome.org>
2019-09-20 02:11:54 +0700

    II->ii

--- crow-3.lua
+++ crow-3.lua
@@ -11,8 +11,8 @@ scale = m.generate_scale(0,"minor",2)
 function init()
   -- keep the line below only if you don't have a 
   -- powered busboard or teletype connected to your ii network
-  crow.II.pullup(true)
-  crow.II.jf.mode(1)
+  crow.ii.pullup(true)
+  crow.ii.jf.mode(1)
   screen.level(15)
   screen.aa(0)
   screen.line_width(1)
@@ -28,11 +28,11 @@ end
 function key(n,z)
   if n==2 and z==1 then
     note = note % (#scale) + 1
-    crow.II.jf.play_note(scale[note]/12-1,math.random(5)+1)
+    crow.ii.jf.play_note(scale[note]/12-1,math.random(5)+1)
     redraw()
   elseif n==3 and z==1 then
     note = math.random(#scale)
-    crow.II.jf.play_note(scale[note]/12-1,math.random(5)+1)
+    crow.ii.jf.play_note(scale[note]/12-1,math.random(5)+1)
     redraw()
   end
 end 
